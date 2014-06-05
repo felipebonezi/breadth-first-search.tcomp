22cb1723d28cf1f768d706d56824cff24d8c360d
Felipe Bonezi <felipebonezi@gmail.com>
2014-06-05 19:11:45 -0300

    Models

---
+++ src/br/com/fugb/ecomp/tcomp/models/Edge.java
@@ -0,0 +1,61 @@
+package br.com.fugb.ecomp.tcomp.models;
+
+public class Edge {
+
+	private static final String DEFAULT_IDENTIFIER = "DIJKSTRA";
+
+	/**
+	 * Identificador da aresta.
+	 */
+	private String identifier;
+	
+	/**
+	 * Ponto inicial.
+	 */
+	private Vertex initial;
+	
+	/**
+	 *	Ponto de destino - final.
+	 */
+	private Vertex destination;
+	
+	/**
+	 * O peso na utilização da aresta entre A e B.
+	 * Podemos considerar várias unidades, mas neste exemplo
+	 * iremos utilizar o conceito de kilometros.
+	 */
+	private int weight;
+	
+	public Edge(Vertex initial, Vertex destination, int weight) {
+		this(DEFAULT_IDENTIFIER, initial, destination, weight);
+	}
+	
+	public Edge(String identifier, Vertex initial, Vertex destination, int weight) {
+		this.identifier = identifier;
+		this.initial = initial;
+		this.destination = destination;
+		this.weight = weight;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public Vertex getInitial() {
+		return initial;
+	}
+
+	public Vertex getDestination() {
+		return destination;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+	
+	@Override
+	public String toString() {
+		return "[" + this.identifier + "] " + this.initial + " to " + this.destination + " using " + this.weight + "km.";
+	}
+	
+}
---
+++ src/br/com/fugb/ecomp/tcomp/models/Vertex.java
@@ -0,0 +1,46 @@
+package br.com.fugb.ecomp.tcomp.models;
+
+
+/**
+ * Classe responsável por guardar a referência do Vértice utilizado no Grafo.
+ * @author Felipe Bonezi <felipebonezi@gmail.com>
+ *
+ */
+public class Vertex {
+
+	/**
+	 * Identificador utilizado na impressão do nome do vértice.
+	 */
+	private String identifier;
+	
+	public Vertex(String identifier) {
+		this.identifier = identifier;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Vertex other = (Vertex) obj;
+		if (identifier == null) {
+			if (other.identifier != null)
+				return false;
+		} else if (!identifier.equals(other.identifier))
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return this.identifier;
+	}
+	
+}
